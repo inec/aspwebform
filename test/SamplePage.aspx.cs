dc30ce5c76569beec1c1463e14e5ba782ab509b4
Y-Chi Lu <luyege+github@gmail.com>
2017-05-17 00:14:12 -0500

    'total'

--- test/SamplePage.aspx.cs
+++ test/SamplePage.aspx.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Web;
 using System.Web.UI;
-using System.Web.Http;
+using System.Net.Http;
 using System.Web.UI.WebControls;
+using System.Text;
 public partial class SamplePage : System.Web.UI.Page
 {
     protected void Button1_Click(object sender, EventArgs e)
@@ -13,11 +14,12 @@ public partial class SamplePage : System.Web.UI.Page
      [System.Web.Services.WebMethod]
         public static string GetCurrentTime(string name)
         {
-
+            var client = new HttpClient();
 string url="https://jsonplaceholder.typicode.com"; //jsonObject.ToString()
-            var content = new StringContent("title=foo&body=bar&userId=1", Encoding.UTF8, "application/json");
+       var content = new StringContent("title=foo&body=bar&userId=1",
+        Encoding.UTF8, "application/json");
 var result = client.PostAsync(url, content).Result;
-return result.toString();
-            //return "Hello " + name + Environment.NewLine + "The Current Time is: "                + DateTime.Now.ToString();
+return result.Content.ToString();
+//  return "Hello " + name + Environment.NewLine + "The Current Time is: "                + DateTime.Now.ToString();
         }
 }
